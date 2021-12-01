044f02be89926d5458b6f0f8179295ea6ade3085
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-12-01 10:46:55 +0000

    redirecting

--- app.rb
+++ app.rb
@@ -1,21 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 class Battle < Sinatra::Base
+	enable :sessions
+	
 	configure :development do
 		register Sinatra::Reloader
 	end
 
 	get '/' do
-		erb(:index)
+		erb :index	
 	end
 
 	post '/names' do
-		p params
-		@player_1_name = params[:player_1_name]
-		@player_2_name = params[:player_2_name]
-		erb(:index)
+		session[:player_1_name] = params[:player_1_name]
+		session[:player_2_name] = params[:player_2_name]
+		redirect '/play'
+	end
+
+	get '/play' do
+		@player_1_name = session[:player_1_name]
+		@player_2_name = session[:player_2_name]
+		erb :play
 	end
 
 	run! if app_file == $0
